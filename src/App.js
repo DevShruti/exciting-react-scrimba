1340d75664d95582e3386d8ef955cfa80f36f3dd
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-15 11:46:57 -0600

    main content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 
 import './App.css';
 import Header from './Header.js';
-import Footer from './Footer.js'
+import Footer from './Footer.js';
+import MainContent from './MainContent.js';
 
 
-function MainContent() {
-  return (
-      <div>
-          <h1>Reasons I'm excited to learn React</h1>
-          <ol>
-              <li>It's a popular library, so I'll be 
-              able to fit in with the cool kids!</li>
-              <li>I'm more likely to get a job as a developer
-              if I know React</li>
-          </ol>
-      </div>
-  )
-}
 
 function Page() {
   return (
